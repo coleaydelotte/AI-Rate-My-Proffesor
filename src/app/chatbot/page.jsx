ed3b1240e055732f6fbd68404867610ea71ad166
Francisco <figueroaf@moravian.edu>
2024-08-19 20:31:49 -0400

    added routing, when clicking on the start button you will open page from the chatbot folder, must keep the jsx file called page in order to work, can't rename it

---
+++ src/app/chatbot/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Bot = () => {
+  return (
+    <div>
+      <h1>test</h1>
+      
+    </div>
+  )
+}
+
+export default Bot
--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import heroLogo from './assets/herologo.png'
 import { useEffect } from 'react';
 import slideUp from "./animations/slideUp";
 import slideFromLeft from "./animations/slideFromLeft";
+import Link from "next/link";
 
 
 
@@ -21,7 +22,10 @@ export default function Home() {
         <div className="home-text slideLeftNone">
           <h1 className="title" >Rate My Professor</h1>
           <p className="subtitle">Rate My Professor is a platform where students can rate professors using an AI-powered chatbot. Provide your insights, and the chatbot will generate a detailed rating based on your input. It's a quick and easy way to help others choose the right classes and share your academic experiences.</p>
+          <Link href="/chatbot">
           <button className="started-btn">Get Started</button>
+          </Link>
+
         </div>
 
         <div className="home-hero">
